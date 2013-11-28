223403d1826ef5ea9ffddef89724ca383412f38c
John Chilton <jmchilton@gmail.com>
2013-11-28 12:24:24 -0600

    Refactor a Downloader interface out of Download properties.
    Again, this will enable the implementation of a direct github zip download.

--- src/main/java/com/github/jmchilton/galaxybootstrap/DownloadProperties.java
+++ src/main/java/com/github/jmchilton/galaxybootstrap/DownloadProperties.java
@@ -16,18 +16,12 @@ public class DownloadProperties {
   public static final String BRANCH_STABLE = "stable";
   public static final String BRANCH_DEFAULT = "default";
   private static final String DEFAULT_REPOSITORY_URL = GALAXY_DIST_REPOSITORY_URL;
-  final String branch;
-  final String repositoryUrl;
+  private final Downloader downloader;
   final File location;
   boolean cache = true;
 
-  public DownloadProperties(final String repositoryUrl, final File location) {
-    this(repositoryUrl, BRANCH_STABLE, location);
-  }
-  
-  public DownloadProperties(final String repositoryUrl, final String branch, final File location_) {
-    this.repositoryUrl = repositoryUrl;
-    this.branch = branch;
+  DownloadProperties(final Downloader downloader, final File location_) {
+    this.downloader = downloader;
     File location = location_;
     if(location == null) {
       try {
@@ -38,7 +32,15 @@ public class DownloadProperties {
         throw new RuntimeException(ex);
       }
     }
-    this.location = location;
+    this.location = location;    
+  }
+  
+  public DownloadProperties(final String repositoryUrl, final File location) {
+    this(repositoryUrl, BRANCH_STABLE, location);
+  }
+  
+  public DownloadProperties(final String repositoryUrl, final String branch, final File location) {
+    this(new HgDownloader(repositoryUrl, branch), location);
   }
 
   public DownloadProperties(final String repositoryUrl) {
@@ -59,31 +61,64 @@ public class DownloadProperties {
   
   public static DownloadProperties forGalaxyCentral() {
     return new DownloadProperties(GALAXY_CENTRAL_REPOSITORY_URL, BRANCH_DEFAULT, null);
-  }  
+  }
 
   void download() {
     final String path = location.getAbsolutePath();
-    String repositoryTarget = repositoryUrl;
-    if(cache) {
-      final String repoHash = Hashing.md5().hashString(repositoryUrl).toString();
-      final File cache = new File(Config.home(), repoHash);
-      if(!cache.exists()) {
-        cache.getParentFile().mkdirs();
-        IoUtils.executeAndWait("hg", "clone", repositoryUrl, cache.getAbsolutePath());
-      }
-      IoUtils.executeAndWait("hg", "-R", cache.getAbsolutePath(), "pull", "-u");
-      repositoryTarget = cache.getAbsolutePath();
-    }
-    final List<String> cloneCommand = new ArrayList<String>();
-    cloneCommand.add("hg");
-    cloneCommand.add("clone");
-    if(branch != null) {
-      cloneCommand.add("-b");
-      cloneCommand.add(branch);
-    }
-    cloneCommand.add(repositoryTarget);
-    cloneCommand.add(path);
-    IoUtils.executeAndWait(cloneCommand.toArray(new String[0]));
+    this.downloader.downlaodTo(location, cache);
   }
   
+
+  private interface Downloader {
+
+    void downlaodTo(File path, boolean useCache);
+
+  }
+
+  private static class HgDownloader implements Downloader {
+    private final String branch;
+    private final String repositoryUrl;
+
+    
+    HgDownloader(final String repositoryUrl, final String branch) {
+      this.branch = branch;
+      this.repositoryUrl = repositoryUrl;
+    }
+
+    
+    @Override
+    public void downlaodTo(File path, boolean useCache) {
+      String repositoryTarget = repositoryUrl;
+      if(useCache) {
+        final String repoHash = Hashing.md5().hashString(repositoryUrl).toString();
+        final File cache = new File(Config.home(), repoHash);
+        if(!cache.exists()) {
+          cache.getParentFile().mkdirs();
+          IoUtils.executeAndWait("hg", "clone", repositoryUrl, cache.getAbsolutePath());
+        }
+        IoUtils.executeAndWait("hg", "-R", cache.getAbsolutePath(), "pull", "-u");
+        repositoryTarget = cache.getAbsolutePath();
+      }
+      final List<String> cloneCommand = new ArrayList<String>();
+      cloneCommand.add("hg");
+      cloneCommand.add("clone");
+      if(branch != null) {
+        cloneCommand.add("-b");
+        cloneCommand.add(branch);
+      }
+      cloneCommand.add(repositoryTarget);
+      cloneCommand.add(path.getAbsolutePath());
+      IoUtils.executeAndWait(cloneCommand.toArray(new String[0]));
+    }
+
+  }
+
+  private static class GithubDownloader implements Downloader {
+
+    @Override
+    public void downlaodTo(final File path, final boolean useCache) {
+      throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+  }  
 }
