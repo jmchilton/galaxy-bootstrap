93faad7e2c2155a53341679161e65a09376b3207
Dan Fornika <dfornika@gmail.com>
2017-10-26 21:02:14 -0700

    Replace methods that I'd removed, deprecate instead of removing

--- src/main/java/com/github/jmchilton/galaxybootstrap/DownloadProperties.java
+++ src/main/java/com/github/jmchilton/galaxybootstrap/DownloadProperties.java
@@ -3,9 +3,14 @@ package com.github.jmchilton.galaxybootstrap;
 import com.google.common.hash.Hashing;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,19 +26,24 @@ public class DownloadProperties {
   public static final String GITHUB_ZIP_URL = "https://codeload.github.com/galaxyproject/galaxy/zip/";
   public static final String GITHUB_ZIP_MASTER_URL = GITHUB_ZIP_URL+ "master";
   public static final String GALAXY_GITHUB_REPOSITORY_URL = "https://github.com/galaxyproject/galaxy.git";
-  public static final String BRANCH_DEFAULT = "master";
+  public static final String GALAXY_DIST_REPOSITORY_URL = "https://bitbucket.org/galaxy/galaxy-dist";
+  public static final String GALAXY_CENTRAL_REPOSITORY_URL = "https://bitbucket.org/galaxy/galaxy-central";
+  public static final String BRANCH_STABLE = "stable";
+  public static final String BRANCH_DEFAULT = "default";
+  public static final String BRANCH_MASTER= "master";
   public static final String BRANCH_RELEASE_17_01 = "release_17.01";
-  public static final String TAG_RELEASE_17_01 = "v7.01";
   public static final String BRANCH_RELEASE_17_05 = "release_17.05";
+  public static final String TAG_RELEASE_17_01 = "v17.01";
   public static final String TAG_RELEASE_17_05 = "v17.05";
-  public static final String BRANCH_RELEASE_17_09 = "release_17.09";
   public static final String BRANCH_RELEASE_LATEST = BRANCH_RELEASE_17_05;
   public static final String TAG_RELEASE_LATEST = TAG_RELEASE_17_05;
 
   /**
    * Defines a constant for specifying that Galaxy should be downloaded with the latest commit.
    */
+  public static final String LATEST_REVISION = "";
   public static final String LATEST_COMMIT = "";
+
 
   private static final String DEFAULT_REPOSITORY_URL = GALAXY_GITHUB_REPOSITORY_URL;
 
@@ -147,7 +157,71 @@ public class DownloadProperties {
   public static DownloadProperties wgetGithubDev() {
     return wgetGithubDev(null);
   }
-  
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from github using wget.
+   * @return  A DownloadProperties for downloading Galaxy from github using wget.
+   */
+  @Deprecated
+  public static DownloadProperties wgetGithubCentral() {
+    return wgetGithubCentral(null);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from galaxy-dist.
+   * @return A new DownloadProperties for downloading Galaxy from galaxy-dist.
+   */
+  public static DownloadProperties forGalaxyDist() {
+    return forGalaxyDist(null, LATEST_REVISION);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from galaxy-dist with a specific revision.
+   * @param revision The revision to use for Galaxy.
+   * @return A new DownloadProperties for downloading Galaxy from galaxy-dist.
+   */
+  @Deprecated
+  public static DownloadProperties forGalaxyDistAtRevision(String revision) {
+    return forGalaxyDist(null, revision);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from galaxy-central.
+   * @return A new DownloadProperties for downloading Galaxy from galaxy-central.
+   */
+  @Deprecated
+  public static DownloadProperties forGalaxyCentral() {
+    return forGalaxyCentral(null, LATEST_REVISION);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from galaxy-central with a specific revision.
+   * @param revision The revision to use for Galaxy.
+   * @return A new DownloadProperties for downloading Galaxy from galaxy-central.
+   */
+  public static DownloadProperties forGalaxyCentralAtRevision(String revision) {
+    return forGalaxyCentral(null, revision);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading the latest stable Galaxy.
+   * @return A new DownloadProperties for downloading the latest stable Galaxy.
+   */
+  @Deprecated
+  public static DownloadProperties forLatestStable() {
+    return forLatestStable(null);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading the latest stable Galaxy with a specific revision.
+   * @param revision The revision to use for Galaxy.
+   * @return A new DownloadProperties for downloading the latest stable Galaxy.
+   */
+  @Deprecated
+  public static DownloadProperties forStableAtRevision(String revision) {
+    return forStableAtRevision(null, revision);
+  }
+
   /**
    * Builds a new DownloadProperties for downloading the latest stable Galaxy.
    * @return A new DownloadProperties for downloading the latest stable Galaxy.
@@ -227,7 +301,75 @@ public class DownloadProperties {
   public static DownloadProperties gitGithub(final String branch, final File destination) {
     return new DownloadProperties(new GitGithubDownloader(GALAXY_GITHUB_REPOSITORY_URL, branch, LATEST_COMMIT), destination);
   }
-  
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from galaxy-dist.
+   * @param destination The destination directory to store Galaxy, null if a directory
+   *  should be chosen by default.
+   * @return A new DownloadProperties for downloading Galaxy from galaxy-dist.
+   */
+  @Deprecated
+  public static DownloadProperties forGalaxyDist(final File destination) {
+    return new DownloadProperties(GALAXY_DIST_REPOSITORY_URL, BRANCH_STABLE, destination);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from galaxy-dist.
+   * @param destination The destination directory to store Galaxy, null if a directory
+   *  should be chosen by default.
+   * @param revision The revision to use for Galaxy.
+   * @return A new DownloadProperties for downloading Galaxy from galaxy-dist.
+   */
+  @Deprecated
+  public static DownloadProperties forGalaxyDist(final File destination, String revision) {
+    return new DownloadProperties(GALAXY_DIST_REPOSITORY_URL, BRANCH_STABLE, revision, destination);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from galaxy-central.
+   * @param destination The destination directory to store Galaxy, null if a directory
+   *  should be chosen by default.
+   * @return A new DownloadProperties for downloading Galaxy from galaxy-central.
+   */
+  @Deprecated
+  public static DownloadProperties forGalaxyCentral(final File destination) {
+    return new DownloadProperties(GALAXY_CENTRAL_REPOSITORY_URL, BRANCH_DEFAULT, destination);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy from galaxy-central.
+   * @param destination The destination directory to store Galaxy, null if a directory
+   *  should be chosen by default.
+   * @param revision The revision to use for Galaxy.
+   * @return A new DownloadProperties for downloading Galaxy from galaxy-central.
+   */
+  @Deprecated
+  public static DownloadProperties forGalaxyCentral(final File destination, String revision) {
+    return new DownloadProperties(GALAXY_CENTRAL_REPOSITORY_URL, BRANCH_DEFAULT, revision, destination);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading the latest stable Galaxy.
+   * @param destination The destination directory to store Galaxy, null if a directory
+   *  should be chosen by default.
+   * @return A new DownloadProperties for downloading the latest stable Galaxy.
+   */
+  @Deprecated
+  public static DownloadProperties forLatestStable(final File destination) {
+    return new DownloadProperties(GALAXY_CENTRAL_REPOSITORY_URL, BRANCH_STABLE, destination);
+  }
+
+  /**
+   * Builds a new DownloadProperties for downloading Galaxy at the given revision.
+   * @param destination The destination directory to store Galaxy, null if a directory
+   *  should be chosen by default.
+   * @return A new DownloadProperties for downloading Galaxy at the given revision.
+   */
+  @Deprecated
+  public static DownloadProperties forStableAtRevision(final File destination, String revision) {
+    return new DownloadProperties(GALAXY_CENTRAL_REPOSITORY_URL, BRANCH_STABLE, revision, destination);
+  }
+
   /**
    * Builds a new DownloadProperties for downloading the latest stable Galaxy.
    * @param destination The destination directory to store Galaxy, null if a directory
@@ -273,7 +415,82 @@ public class DownloadProperties {
     void downloadTo(File path, boolean useCache);
 
   }
-  
+
+  /**
+   *  Defines a downloader to download Galaxy from Mercurial.
+   */
+  @Deprecated
+  private static class HgDownloader implements Downloader {
+    private final String branch;
+    private final String repositoryUrl;
+    private final File cacheDir;
+
+    /**
+     * Revision to checkout, if null assumes we are interested in most recent.
+     */
+    private final String revision;
+
+    public HgDownloader(final String repositoryUrl, final String branch, final String revision) {
+      if (revision == null) {
+        throw new IllegalArgumentException("revision is null");
+      }
+
+      this.branch = branch;
+      this.repositoryUrl = repositoryUrl;
+      this.revision = revision;
+
+      this.cacheDir = getCacheDir(repositoryUrl);
+    }
+
+    /**
+     * Gets the directory of the Galaxy repository cache.
+     * @param repositoryUrl  The url used to constructe the cache directory.
+     * @return  The Galaxy repository cache.
+     */
+    private File getCacheDir(String repositoryUrl) {
+      final String repoHash = Hashing.md5().hashUnencodedChars(repositoryUrl).toString();
+      final File cache = new File(Config.home(), repoHash);
+
+      return cache;
+    }
+
+    @Override
+    public void downloadTo(File path, boolean useCache) {
+      String repositoryTarget = repositoryUrl;
+      if(useCache) {
+        if(!cacheDir.exists()) {
+          cacheDir.getParentFile().mkdirs();
+          IoUtils.executeAndWait("hg", "clone", repositoryUrl, cacheDir.getAbsolutePath());
+        }
+        IoUtils.executeAndWait("hg", "-R", cacheDir.getAbsolutePath(), "pull", "-u");
+        repositoryTarget = cacheDir.getAbsolutePath();
+      }
+      final List<String> cloneCommand = new ArrayList<String>();
+      cloneCommand.add("hg");
+      cloneCommand.add("clone");
+      if(branch != null) {
+        cloneCommand.add("-b");
+        cloneCommand.add(branch);
+      }
+
+      if (!LATEST_REVISION.equals(revision)) {
+        cloneCommand.add("-r");
+        cloneCommand.add(revision);
+      }
+
+      cloneCommand.add(repositoryTarget);
+      cloneCommand.add(path.getAbsolutePath());
+      IoUtils.executeAndWait(cloneCommand.toArray(new String[0]));
+    }
+
+    @Override
+    public String toString() {
+      String revision = (LATEST_REVISION.equals(this.revision) ? "latest" : this.revision);
+      return "Mercurial [repositoryUrl=" + repositoryUrl + ", branch="
+              + branch + ", revision=" + revision + ", cacheDir=" + cacheDir.getAbsolutePath() +"]";
+    }
+  }
+
   /**
    * Defines a downloader to download Galaxy from GitHub with wget.
    */
@@ -385,4 +602,55 @@ public class DownloadProperties {
     }
   }
 
+  /**
+   * Defines a downloader to download Galaxy from github using Java.
+   */
+  @SuppressWarnings("unused")
+  @Deprecated
+  private static class JavaGithubDownloader implements Downloader {
+    // WAY TO SLOW.
+    @Override
+    public void downloadTo(final File path, final boolean useCache) {
+
+      try {
+        final URL download = new URL(GITHUB_ZIP_MASTER_URL);
+        ZipInputStream zis = new ZipInputStream(download.openStream());
+        ZipEntry ze = zis.getNextEntry();
+        byte[] buffer = new byte[1024];
+        while(ze!=null){
+
+          String fileName = ze.getName().substring("galaxy-central-master/".length());
+          if(fileName.equals("")) {
+            continue;
+          }
+          File newFile = new File(path + File.separator + fileName);
+
+          //create all non exists folders
+          //else you will hit FileNotFoundException for compressed folder
+          new File(newFile.getParent()).mkdirs();
+
+          FileOutputStream fos = new FileOutputStream(newFile);
+
+          int len;
+          while ((len = zis.read(buffer)) > 0) {
+            fos.write(buffer, 0, len);
+          }
+
+          fos.close();
+          ze = zis.getNextEntry();
+        }
+
+        zis.closeEntry();
+        zis.close();
+
+      } catch(IOException ex) {
+        throw new RuntimeException(ex);
+      }
+    }
+
+    @Override
+    public String toString() {
+      return "JavaGithubDownloader [url=" + GITHUB_ZIP_MASTER_URL + ", branch=master]";
+    }
+  }
 }
